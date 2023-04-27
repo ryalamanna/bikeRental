5cee87414f7d738faf48039c21cdaf2c0c694770
Ryal rafter amanna <ryalamanna@gmail.com>
2023-04-27 22:37:57 +0530

    database setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,21 +2,30 @@ import express, { Request, Response, Application, NextFunction, ErrorRequestHand
 import { Server } from 'http';
 import createHttpError from 'http-errors';
 import {config} from 'dotenv'
+import {db} from '../db_config'
 const app: Application = express();
 config();
-app.use((req: Request, res: Response, next: NextFunction) => {
-    next(new createHttpError.NotFound())
-})
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//     next(new createHttpError.NotFound())
+// })
 
-const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-    res.status(err.status || 500)
-    res.send({
-        status: err.status || 500,
-        message: err.message
-    })
-}
+// const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+//     res.status(err.status || 500)
+//     res.send({
+//         status: err.status || 500,
+//         message: err.message
+//     })
+// }
 app.get('/', (req: Request, res: Response) => {
-    res.send('hiiiiiii')
+    
+    db.query('SELECT * FROM users', (err, result) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.send(result)
+        }
+    })
 })
 
 const port : number = Number(process.env.por || 3000);
